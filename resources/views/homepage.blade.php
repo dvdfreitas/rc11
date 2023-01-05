346ba3b175ac39462a56a3c41835f44df245b41e
David Freitas <dvdfreitas@gmail.com>
2023-01-05 12:04:54 +0000

    230105 12:00 Adicionei menu

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,3 +1,3 @@
 <x-guestLayout>
-    <img src="https://files.app.fcporto.pt/sources/5c90c1275b9baxXEyzMpfKo1ryKNJ.jpg">
+    
 </x-guestLayout>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -4,8 +4,16 @@
         <script src="https://cdn.tailwindcss.com"></script>
     </head>
     <body>
-        <div class="max-w-xl bg-blue-200 m-auto md:bg-red-400">
-            Homepage | Contactos | Acerca
+        <div class="mx-2 xl:m-auto xl:mt-4 max-w-3xl xl:rounded-xl text-xl xl:flex justify-around gap-2 bg-blue-900 text-white p-4">
+            <div class="hidden xl:inline">|</div>
+            <div><a href="">Homepage</a></div>
+            <div class="hidden xl:inline">|</div>
+            <div><a href="">Quem somos</a></div>
+            <div class="hidden xl:inline">|</div>
+            <div><a href="">A nossa missão</a></div>
+            <div class="hidden xl:inline">|</div>
+            <div><a href="">Contactos</a></div>
+            <div class="hidden xl:inline">|</div>
         </div>
         {{ $slot }}
     </body>
